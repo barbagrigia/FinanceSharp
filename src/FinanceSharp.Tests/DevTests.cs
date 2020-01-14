a192c44264633b63e2a2a5f0f078ff891c692cf0
Eli Belash <elibelash@gmail.com>
2020-01-15 01:31:16 +0200

    Resolved nuget packages

--- src/FinanceSharp.Tests/DevTests.cs
+++ src/FinanceSharp.Tests/DevTests.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using NUnit.Framework;
 using Torch;
-using TorchSharp.Tensor;
 
 namespace FinanceSharp.Tests {
     public class DevTests {
@@ -13,13 +12,6 @@ namespace FinanceSharp.Tests {
             var a = torch.tensor(new int[] {1, 2});
             var b = a + 1;
             Console.WriteLine(b);
-        }
-    }
-
-    public static unsafe class NumSharpExtensions {
-        public static TorchTensor ToDoubleTensor(this NDArray nd) {
-            nd = nd.typecode == NPTypeCode.Double ? nd : nd.astype(NPTypeCode.Double);
-            return TorchSharp.Tensor.DoubleTensor.From((IntPtr) nd.Unsafe.Address, nd.Shape.Dimensions.Select(dim => (long) dim).ToArray(), true);
         }
     }
 }
