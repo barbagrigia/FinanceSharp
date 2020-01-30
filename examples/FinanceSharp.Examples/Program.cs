82d1119ca8231c0ccaf0ea23733661730c96dd1d
Eli Belash <elibelash@gmail.com>
2020-01-30 16:23:33 +0200

    Added more examples

--- examples/FinanceSharp.Examples/Program.cs
+++ examples/FinanceSharp.Examples/Program.cs
@@ -33,31 +33,69 @@ namespace FinanceSharp.Examples {
                         .ToArray();
                 }
             }
-            
+
             //build model
-            var tickCons = new TickConsolidator(TimeSpan.FromMinutes(1d));
-            var ema = new ExponentialMovingAverage(12).Of(tickCons);
+            (IUpdatable Input, IUpdatable[] Outputs, List<DoubleArray>[] Datas) model = HeikinAshi();
 
-            var closes = new List<DoubleArray>();
-            var emas = new List<DoubleArray>();
-
-            //bind model
-            tickCons.Updated += (time, updated) => closes.Add(updated);
-            ema.Updated += (time, updated) => emas.Add(updated);
 
             //feed the model data
-            foreach (var tick in ticks)
-            {
-                tickCons.Update(tick.Time, tick.Value);
+            foreach (var tick in ticks) {
+                model.Input.Update(tick.Time, tick.Value);
             }
 
             //plot
-            Plotter.Show(Enumerable.Range(0, emas.Count).Select(i => (double) i).ToArray(),
-                new DoubleArray[2][] {
-                    closes.ToArray(),
-                    emas.ToArray()
-                }
+            Plotter.Show(
+                Enumerable.Range(0, model.Datas[0].Count).Select(i => (double) i).ToArray(),
+                model.Datas.Select(m => m.ToArray()).ToArray()
             );
+        }
+
+        public static (IUpdatable Input, IUpdatable[] Outputs, List<DoubleArray>[] Datas) EMA() {
+            var tickCons = new TickConsolidator(TimeSpan.FromMinutes(1d));
+            var ema = new ExponentialMovingAverage(12).Of(tickCons);
+
+            var bars = new List<DoubleArray>();
+            var emas = new List<DoubleArray>();
+
+            //bind model
+            tickCons.Updated += (time, updated) => bars.Add(updated);
+            ema.Updated += (time, updated) => emas.Add(updated);
+
+            return (tickCons, new IUpdatable[] {ema}, new List<DoubleArray>[] {bars, emas});
+        }
+
+        public static (IUpdatable Input, IUpdatable[] Outputs, List<DoubleArray>[] Datas) EMAx3() {
+            var tickCons = new TickConsolidator(TimeSpan.FromMinutes(1d));
+            var ema6 = new ExponentialMovingAverage(6).Of(tickCons);
+            var ema12 = new ExponentialMovingAverage(12).Of(tickCons);
+            var ema24 = new ExponentialMovingAverage(24).Of(tickCons);
+
+            var bars = new List<DoubleArray>();
+            var emas6 = new List<DoubleArray>();
+            var emas12 = new List<DoubleArray>();
+            var emas24 = new List<DoubleArray>();
+
+            //bind model
+            tickCons.Updated += (time, updated) => bars.Add(updated);
+            ema6.Updated += (time, updated) => emas6.Add(updated);
+            ema12.Updated += (time, updated) => emas12.Add(updated);
+            ema24.Updated += (time, updated) => emas24.Add(updated);
+
+            return (tickCons, new IUpdatable[] {ema6, ema12, ema24}, new List<DoubleArray>[] {bars, emas6, emas12, emas24});
+        }
+
+        public static (IUpdatable Input, IUpdatable[] Outputs, List<DoubleArray>[] Datas) HeikinAshi() {
+            var tickCons = new TickConsolidator(TimeSpan.FromMinutes(1d));
+            var ha = new HeikinAshi().Of(tickCons);
+
+            var bars = new List<DoubleArray>();
+            var has = new List<DoubleArray>();
+
+            //bind model
+            tickCons.Updated += (time, updated) => bars.Add(updated);
+            ha.Updated += (time, updated) => has.Add(DoubleArray.FromStruct(ha.CurrentBar) + 25d); //note that we append +25 offset so ha will be visible
+
+            return (tickCons, new IUpdatable[] {ha}, new List<DoubleArray>[] {bars, has});
         }
     }
 }
